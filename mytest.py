ae8096fd5af90eac7cad6eb195cf54b7dc3c5379
mpapenbr <Markus.Papenbrock@gmail.com>
2020-04-10 14:44:56 +0200

    Initial commit

---
+++ mytest.py
@@ -0,0 +1,18 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+a = "Hello"
+print(a)
+
+
+# %%
+import os
+cwd = os.getcwd()
+print(os.listdir())
+
+# %% [markdown]
+# # Hallo Markdown
+
+# %%
+
+
